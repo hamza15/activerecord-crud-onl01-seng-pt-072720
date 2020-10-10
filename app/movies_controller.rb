4841044134734d4fd6f12728849f7144f045be69
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-10-10 23:58:48 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,7 +34,8 @@ def can_be_created_in_a_block(args = nil)
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  m.title = args.nil? ? "Home Alone" : args[:title]
+  m.release_date = args.nil? ? 1990 : args[:release_date]
 end
 
 def can_get_the_last_item_in_the_database
